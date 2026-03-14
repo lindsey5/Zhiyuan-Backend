820c8e50ebd7aeb5c12fc6bb2c8cfb37204b0484
lindsey5 <lindseysamson5@gmail.com>
2026-03-14 14:14:03 +0800

    refactor: remove comments

--- models/index.ts
+++ models/index.ts
@@ -2,11 +2,9 @@ import Permission from "./Permission";
 import Role from "./Role";
 import User from "./User";
 
-// User ↔ Role
 Role.hasMany(User, { foreignKey: 'role_id', as: 'users' });
 User.belongsTo(Role, { foreignKey: 'role_id', as: 'role' });
 
-// Role ↔ Permission
 Role.hasMany(Permission, { foreignKey: 'role_id', as: 'permissions' });
 Permission.belongsTo(Role, { foreignKey: 'role_id', as: 'role' });
 
