e5fcb2de96c375017905281e69921766e7ac2c34
lindsey5 <lindseysamson5@gmail.com>
2026-03-17 05:03:21 +0800

    feat: add request body validation middleware in login api

--- routes/authRoutes.ts
+++ routes/authRoutes.ts
@@ -1,9 +1,11 @@
 import { Router } from "express";
 import { login } from "../controllers/authController";
+import validateBody from "../middlewares/validateBody";
+import { loginSchema } from "../schema/loginSchema";
 
 const router = Router();
 
-router.post('/login', login);
+router.post('/login', validateBody(loginSchema), login);
 
 const authRoutes = router
 
