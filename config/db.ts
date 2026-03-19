3fa72bb49ac5326cb726128eff08d799f6d89e8b
lindsey5 <lindsey5@gmail.com>
2026-03-19 12:54:31 +0800

    refactor database config

--- config/db.ts
+++ config/db.ts
@@ -3,9 +3,16 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-const sequelize = new Sequelize({
-  dialect: "sqlite",
-  storage: process.env.DB_PATH
-});
+const sequelize = new Sequelize(
+  process.env.DB!,
+  process.env.DB_USERNAME!,
+  process.env.DB_PASSWORD!,
+  {
+    host: process.env.DB_HOST,
+    dialect: 'mysql',
+    port: process.env.DB_PORT ? Number(process.env.DB_PORT) : 3306,
+    logging: false,
+  }
+);
 
 export default sequelize
