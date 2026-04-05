076594771d55634279d54f23c1f4f8bc1cc3a6c0
lindsey5 <lindseysamson5@gmail.com>
2026-04-06 03:39:58 +0800

    commit

--- sockets/socket.ts
+++ sockets/socket.ts
@@ -3,13 +3,6 @@ import dotenv from 'dotenv';
 import { initStockTransferLogSocket } from "./transferLogSocket";
 dotenv.config();
 
-const allowedOrigins = (process.env.ORIGINS?.split(",") || [
-    "https://zhiyuan-frontend.vercel.app",
-    "https://zhiyuan-distributor.vercel.app",
-    "http://localhost:5173",
-    "http://localhost:5174",
-]);
-
 export function registerSockets(io: Server) {
     initStockTransferLogSocket(io);
 }
@@ -17,10 +10,9 @@ export function registerSockets(io: Server) {
 export default function initializeSocket(server: any) {
     const io = new Server(server, {
         cors: {
-            origin: allowedOrigins, 
+            origin: ['*'],
             methods: ["GET", "POST"],
             allowedHeaders: ["Authorization"],
-            credentials: true,
         },
     });
 
