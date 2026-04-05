1f79ecdbeefefa65a08317844b27484cfbfde3fb
lindsey5 <lindseysamson5@gmail.com>
2026-04-06 03:34:34 +0800

    initial commit

--- sockets/socket.ts
+++ sockets/socket.ts
@@ -3,8 +3,6 @@ import dotenv from 'dotenv';
 import { initStockTransferLogSocket } from "./transferLogSocket";
 dotenv.config();
 
-const origins = process.env.ORIGINS?.split(",") || ['http://localhost:5173'];
-
 export function registerSockets(io: Server) {
     initStockTransferLogSocket(io);
 }
@@ -12,7 +10,7 @@ export function registerSockets(io: Server) {
 export default function initializeSocket(server: any) {
     const io = new Server(server, {
         cors: {
-            origin: origins,
+            origin: ['*'],
             methods: ["GET", "POST"],
             allowedHeaders: ["Authorization"],
             credentials: true,
