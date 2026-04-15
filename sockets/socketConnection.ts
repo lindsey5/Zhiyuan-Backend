288027ccec3dbfd7d9a20272abf2e2de99526a5f
lindseysamson5 <lindseysamson5@gmail.com>
2026-04-15 14:49:42 +0800

    refactor sockets

--- sockets/socketConnection.ts
+++ sockets/socketConnection.ts
@@ -22,26 +22,25 @@ export default function socketConnection({
 }) {
     namespace.on("connection", async (socket: Socket) => {
         try {
-            if(authenticate){
-                const authHeader = socket.handshake.auth.token;
-                if (!authHeader?.startsWith("Bearer ")) {
-                    throw new Error("Access token required");
-                }
-
-                const token = authHeader.split(" ")[1];
-                const decoded = jwt.verify(token, process.env.JWT_ACCESS_SECRET as string) as JwtPayload;
-                const userId = decoded.id || decoded._id;
-
-                const user = await User.findOne({ _id: userId, status: "active" });
-                const distributor = await Distributor.findOne({ _id: userId, status: "active" });
-
-                if (!user && !distributor) {
-                    throw new Error("Unauthorized user");
-                }
-
-                socket.join(userId);
-                console.log(message, userId);
+            const authHeader = socket.handshake.auth.token;
+            if (!authHeader?.startsWith("Bearer ") && authenticate) {
+                throw new Error("Access token required");
             }
+
+            const token = authHeader.split(" ")[1];
+            const decoded = jwt.verify(token, process.env.JWT_ACCESS_SECRET as string) as JwtPayload;
+            const userId = decoded.id || decoded._id;
+
+            const user = await User.findOne({ _id: userId, status: "active" });
+            const distributor = await Distributor.findOne({ _id: userId, status: "active" });
+
+            if (!user && !distributor && authenticate) {
+                throw new Error("Unauthorized user");
+            }
+
+            if(authenticate) socket.join(userId);
+
+            console.log(message, userId || socket.id);
 
             // Register custom events dynamically
             if (events) {
