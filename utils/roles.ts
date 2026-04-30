a7bfa750c51b288e7cc16945645338cec2b8337d
lindseysamson5 <lindseysamson5@gmail.com>
2026-05-01 07:14:28 +0800

    feat: Add 'transfer-logs:view:all' permission to ROLES

--- utils/roles.ts
+++ utils/roles.ts
@@ -60,6 +60,7 @@ const ROLES = {
             'sponsored-items:view:all',
 
             // Transfer Logs
+            'tranfer-logs:view:all',
             'tranfer-logs:view:own',
             'transfer-logs:update',
             'transfer-logs:create',
